1a614952b49533ba0538f110edb51005d02c10f6
Chris Arsenault <carsena2@gmail.com>
2025-11-26 03:13:38 -0700

    infra

---
+++ infrastructure/terraform/variables.tf
@@ -0,0 +1,19 @@
+# variables.tf - Input variables for penguin-tales infrastructure
+
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1" # Required for CloudFront ACM certificates
+}
+
+variable "prefix" {
+  description = "Project prefix for namespacing resources"
+  type        = string
+  default     = "pt"
+}
+
+variable "domain_name" {
+  description = "Domain name for the website"
+  type        = string
+  default     = "penguin-tales.com"
+}
